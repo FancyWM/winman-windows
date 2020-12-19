3078c45253a51cbb7cede6cf53360832eae5a7a7
Veselin Karaganev <vesko.karaganev@gmail.com>
2020-12-19 22:18:55 +0200

    Breaking changes, prep for virtual desktop support

---
+++ WinMan/Implementation/DummyVirtualDesktop.cs
@@ -0,0 +1,31 @@
+﻿namespace WinMan.Implementation
+{
+    internal class DummyVirtualDesktop : IVirtualDesktop
+    {
+        public bool IsCurrent => true;
+
+        public void Add(IWindow window)
+        {
+        }
+
+        public bool Contains(IWindow window)
+        {
+            return window.IsValid;
+        }
+
+        public override string ToString()
+        {
+            return "DummyVirtualDesktop { IsCurrent = true }";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DummyVirtualDesktop;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
