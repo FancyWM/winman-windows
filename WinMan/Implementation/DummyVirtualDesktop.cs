b0f8f196e282a219284585a4ba57646507af35fc
Veselin Karaganev <vesko.karaganev@gmail.com>
2020-12-24 22:52:17 +0200

    Add virtual desktop support

--- WinMan/Implementation/DummyVirtualDesktop.cs
+++ WinMan/Implementation/DummyVirtualDesktop.cs
@@ -1,16 +1,50 @@
-﻿namespace WinMan.Implementation
+﻿using System;
+
+namespace WinMan.Implementation
 {
     internal class DummyVirtualDesktop : IVirtualDesktop
     {
         public bool IsCurrent => true;
 
-        public void Add(IWindow window)
+        public int Index => 0;
+
+        public string Name { get; private set; } = "Main Desktop";
+
+        public IWorkspace Workspace { get; }
+
+        public event VirtualDesktopChangedEventHandler Removed;
+        
+        public DummyVirtualDesktop(IWorkspace workspace)
+        {
+            Workspace = workspace;
+        }
+
+        public void MoveWindow(IWindow window)
         {
         }
 
-        public bool Contains(IWindow window)
+        public bool HasWindow(IWindow window)
         {
             return window.IsValid;
+        }
+
+        public void SwitchTo()
+        {
+        }
+
+        public void SetName(string newName)
+        {
+            Name = newName;
+        }
+
+        public void Remove()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
         }
 
         public override string ToString()
@@ -21,11 +55,6 @@
         public override bool Equals(object obj)
         {
             return obj is DummyVirtualDesktop;
-        }
-
-        public override int GetHashCode()
-        {
-            return 0;
         }
     }
 }
---
+++ WinMan/Implementation/DummyVirtualDesktopManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinMan.Implementation
+{
+    internal class DummyVirtualDesktopManager : IVirtualDesktopManager
+    {
+        public IWorkspace Workspace { get; }
+
+        public bool IsVirtualDesktopsSupported => false;
+
+        public IReadOnlyList<IVirtualDesktop> Desktops { get; }
+
+        public IVirtualDesktop CurrentDesktop => Desktops[0];
+
+
+        public event VirtualDesktopChangedEventHandler CurrentDesktopChanged;
+        public event VirtualDesktopChangedEventHandler DesktopAdded;
+        public event VirtualDesktopChangedEventHandler DesktopRemoved;
+
+        public DummyVirtualDesktopManager(IWorkspace workspace)
+        {
+            Workspace = workspace;
+            Desktops = new[]
+            {
+                new DummyVirtualDesktop(workspace),
+            };
+        }
+
+        public bool IsWindowPinned(IWindow window)
+        {
+            return true;
+        }
+
+        public void PinWindow(IWindow window)
+        {
+        }
+
+        public void UnpinWindow(IWindow window)
+        {
+        }
+
+        public IVirtualDesktop CreateDesktop()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
